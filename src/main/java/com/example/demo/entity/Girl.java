f4c52882d6cfef1dbaf1af590a620eae0990a555
lzha <lzha@trafficcast.com>
2020-04-20 16:46:34 +0800

    dev branch commit

--- src/main/java/com/example/demo/entity/Girl.java
+++ src/main/java/com/example/demo/entity/Girl.java
@@ -1,1 +1,1 @@
-package com.example.demo.entity;public class Girl {    private int id;    private String name1;    private int age1;    private String address1;}
+package com.example.demo.entity;public class Girl {    private int id;    private String name1;    private int age1;    private String address1;    private String devTest;}
